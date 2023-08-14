fea62b2a7c7b241210271abb0e5094a24f4c6ef1
Yuvraj Chaturvedi <96003202+Yuvraj004@users.noreply.github.com>
2023-08-15 00:02:47 +0530

    siteDesigning-1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "y/utils/api";
 import "y/styles/globals.css";
+import Head from "next/head";
+import { SideNav } from "y/components/SideNav";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,7 +12,16 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>Twitter</title>
+        <meta name="description" content="This  is a Twitter clone By yuvraj chaturvedi" />
+      </Head>
+      <div className="container mx-auto flex items-start">
+        <SideNav/>
+        <div className ="min-h-screen flex-grow border-x"></div>
+        <Component {...pageProps} />
+      </div>
+      
     </SessionProvider>
   );
 };
